481436770f6f1b414aee6c5c0319dce193ac25b6
Buttonblue <80797662+Buttonblue@users.noreply.github.com>
2024-03-07 13:39:32 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
+//document.addEventListener("DOMContentLoaded", function() {
+//    setTimeout(function() {
+//        var video = document.getElementById("background-video");
+//        video.style.visibility = "visible";
+//        video.style.opacity = 1;
+//    }, 30000); // 30 segundos de retraso
+//});
 document.addEventListener("DOMContentLoaded", function() {
+    // Mostrar video después de 30 segundos
     setTimeout(function() {
         var video = document.getElementById("background-video");
         video.style.visibility = "visible";
         video.style.opacity = 1;
     }, 30000); // 30 segundos de retraso
+
+    var video = document.getElementById("background-video");
+    var message = document.getElementById("message");
+
+    video.addEventListener("ended", function() {
+        // Cuando el video termina, ocultamos el video y mostramos el mensaje
+        video.style.display = "none";
+        message.classList.remove("hidden");
+    });
 });
